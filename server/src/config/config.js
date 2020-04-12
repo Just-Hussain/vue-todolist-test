d405559c484d94898b653e5743c5849ded4a494b
just-hussain <1just.hussain@gmail.com>
2020-04-12 16:37:13 +0300

    seperating configs

---
+++ server/src/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    port: 8082,
+    db: {
+        host: 'localhost',
+        user: 'root',
+        password: '0000',
+        database: 'Todos'
+    }
+}
--- server/src/index.js
+++ server/src/index.js
@@ -1,38 +1,35 @@
 // whole code is wrapped in an IIFE to use async/await
 (async () => {
 
-// express is what handles all the HTTP work
-const express = require('express')
+    // express is what handles all the HTTP work
+    const express = require('express')
 
-// mysql promise wrapper
-const mysql = require('promise-mysql')
+    // mysql promise wrapper
+    const mysql = require('promise-mysql')
 
-// for orginization, all routes are in a seperate file
-const routes = require('./routes')
+    // for orginization, all routes are in a seperate file
+    const routes = require('./routes')
+    // and all configs too
+    const config = require('./config/config')
 
-// init express
-let app = express()
+    // init express
+    let app = express()
 
-// config for mysql connection
-let config = {
-    host: 'localhost',
-    user: 'root',
-    password: '0000',
-    database: 'Todos'
-}
-// create connection, connecting is done with the first query
-let conn = await mysql.createConnection(config)
+    // config for mysql connection
+    // create connection, connecting is done with the first query
+    let conn = await mysql.createConnection(config.db)
 
-// test query
-let res = await conn.query('SELECT * FROM Todo')
-console.log(res);
+    // test query
+    let res = await conn.query('SELECT * FROM Todo')
+    console.log(res);
 
 
-// pass express instance to <routes> for it to handle GET and POST and such
-routes(app, conn)
+    // pass express instance to <routes> for it to handle GET and POST and such
+    // and the conncetion for mysql queries
+    routes(app, conn)
 
-// start the server on port 8082
-app.listen(8082)
+    // start the server on port port
+    app.listen(config.port)
 
-console.log('up and running');
+    console.log('up and running');
 })()
